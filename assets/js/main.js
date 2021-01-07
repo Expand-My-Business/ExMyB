afbbb55f36c6fda9ba34f2096ed6abd4b4f631d3
uidev-in <mail2loveseries@gmail.com>
2021-01-07 17:16:42 +0530

    added upload company logo field in vendor form step 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -266,3 +266,17 @@ $(function () {
           }); */
       });
 
+
+
+// Image File Upload
+var input = document.getElementById( 'upload' );
+var infoArea = document.getElementById( 'upload-label' );
+
+input.addEventListener( 'change', showFileName );
+function showFileName( event ) {
+  var input = event.srcElement;
+  var fileName = input.files[0].name;
+  infoArea.textContent = 'File : ' + fileName;
+}
+
+
