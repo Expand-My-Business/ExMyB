04c839976188c4e7f27397f5aef28118e4592e84
uidev-in <mail2loveseries@gmail.com>
2021-01-22 19:09:22 +0530

    Tywriter speed increased on index page

--- assets/vendor/typewriter/typewriter.js
+++ assets/vendor/typewriter/typewriter.js
@@ -3,7 +3,7 @@
     this.toRotate = toRotate;
     this.el = el;
     this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
+    this.period = parseInt(period, 10) || 1000;
     this.txt = '';
     this.tick();
     this.isDeleting = false;
@@ -14,9 +14,11 @@
     var fullTxt = this.toRotate[i];
   
     if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
+      /* this.txt = fullTxt.substring(0, this.txt.length - 1); */
+      this.txt = fullTxt.substring(0, this.txt.length - 3);
     } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
+      /* this.txt = fullTxt.substring(0, this.txt.length + 1); */
+      this.txt = fullTxt.substring(0, this.txt.length + 3);
     }
   
     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
@@ -52,6 +54,6 @@
     // INJECT CSS
     var css = document.createElement("style");
     css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #fff }";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid transparent  }";
     document.body.appendChild(css);
   };
